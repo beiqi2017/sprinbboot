b93b834aa7cad4a6b43e46ea525f9b800b32d06a
041597 <041597@sz-041597-nb01.chinatopcredit.com>
2017-06-12 16:43:44 +0800

    commit

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import javax.servlet.http.HttpSession;
 import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.subject.Subject;
 import org.springframework.stereotype.Controller;
@@ -17,16 +18,30 @@ public class LoginController {
     }
     @RequestMapping("/loginUser")
     public String loginUser(String username,String password,HttpSession session) {
-        UsernamePasswordToken usernamePasswordToken=new UsernamePasswordToken(username,password);
-        Subject subject = SecurityUtils.getSubject();
+    	 Subject subject = SecurityUtils.getSubject();
+ 	    UsernamePasswordToken token = new UsernamePasswordToken(username,password);
+ 	    token.setRememberMe(true);
         try {
-            subject.login(usernamePasswordToken);   //完成登录
+        	subject.login(token);
             User user=(User) subject.getPrincipal();
             session.setAttribute("user", user);
             return "index";
-        } catch(Exception e) {
-            return "login";//返回登录页面
-        }
+        }catch (AuthenticationException e) {
+  	      token.clear();
+  	      e.printStackTrace();
+  	      String msg = "";
+             if (e instanceof org.apache.shiro.authc.ConcurrentAccessException) {
+                  msg = "用户已登录！";
+               } else if (e instanceof org.apache.shiro.authc.AccountException) {
+                  msg = "未知帐号错误或用户状态异常！";
+              } else if (e instanceof org.apache.shiro.authc.IncorrectCredentialsException) {
+                  msg = "用户名密码错误！";
+               } else if (e instanceof org.apache.shiro.authc.AuthenticationException) {
+                  msg = "认证失败！";
+               }
+              System.out.println(msg);
+  	          return "login";
+  	    } 
         
     }
     @RequestMapping("/logOut")
