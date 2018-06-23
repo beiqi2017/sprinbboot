39a028a9572292d2a31c37c7f6007621e17b23be
jzb <jzb@LAPTOP-3B8MTSLQ>
2018-06-23 21:47:12 +0800

    提交

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -9,11 +9,17 @@ import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @RestController
 public class HelloController {
@@ -32,8 +38,27 @@ public class HelloController {
         return userService.find(); 
     }
     
-   
-    
+    @ResponseBody
+    @RequestMapping(value = "/session")
+    public Map<String, Object> getSession(HttpServletRequest request) {
+        request.getSession().setAttribute("username", "admin");
+        request.getSession().setAttribute("test", "111");
+        request.getSession().setAttribute("12", "a");
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("sessionId", request.getSession().getId());
+        return map;
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/test")
+    public Map<String, Object> get(HttpServletRequest request) {
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("username", request.getSession().getAttribute("username"));
+        map.put("test", request.getSession().getAttribute("test"));
+        map.put("12", request.getSession().getAttribute("12"));
+        map.put("sessionId", request.getSession().getId());
+        return map;
+    }
     
     
 }
